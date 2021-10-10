f9c36107c07720b845e7305e66e85737ee988549
clearsky77 <clearsky3264@gmail.com>
2021-10-10 13:36:35 +0900

    리스트뷰 이벤트 처리 작업중

---
+++ app/src/main/java/com/clearsky77/pizzaorderapp20211010/adapters/PizzaStoreAdapter.kt
@@ -0,0 +1,42 @@
+package com.clearsky77.pizzaorderapp20211010.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.clearsky77.pizzaorderapp20211010.R
+import com.clearsky77.pizzaorderapp20211010.datas.StoreData
+
+class PizzaStoreAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StoreData>
+) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = mInflater.inflate(R.layout.pizza_store_list_item, null)
+        }
+        val row = tempRow!!
+
+        val data = mList[position]
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg) //이미지뷰로 담아서 쓴다. (circleImageView는 ImageView의 자식이다)
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt )
+
+        storeNameTxt.text = data.name
+
+        Glide.with(mContext).load(data.logoURL).into(logoImg)
+
+
+
+        return  row
+    }
+
+}
