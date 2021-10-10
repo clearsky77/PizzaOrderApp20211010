bd68bc078cae8783fce22a314a0749ff2a775630
clearsky77 <clearsky3264@gmail.com>
2021-10-10 14:20:18 +0900

    닉네임 가지러 이동

--- app/src/main/java/com/clearsky77/pizzaorderapp20211010/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/clearsky77/pizzaorderapp20211010/fragments/MyProfileFragment.kt
@@ -1,13 +1,18 @@
 package com.clearsky77.pizzaorderapp20211010.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.clearsky77.pizzaorderapp20211010.EditNicknameActivity
 import com.clearsky77.pizzaorderapp20211010.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 class MyProfileFragment : Fragment() {
+    val REQ_FOR_NICKNAME = 1010
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +23,11 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        editNicknameBtn.setOnClickListener {
+            val myIntent = Intent(requireContext(), EditNicknameActivity :: class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
+        }
 
 
     }
