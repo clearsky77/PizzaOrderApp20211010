c46d4f5ef108a3d35aae0e346fd8fbc7fde84502
clearsky77 <clearsky3264@gmail.com>
2021-10-10 10:49:14 +0900

    메인화면의 뷰페이저에서 사용할 어댑터 작성  - return when 문법 활용

---
+++ app/src/main/java/com/clearsky77/pizzaorderapp20211010/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.clearsky77.pizzaorderapp20211010.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.clearsky77.pizzaorderapp20211010.fragments.MyProfileFragment
+import com.clearsky77.pizzaorderapp20211010.fragments.PizzaStoreListFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+    }
+
+}
